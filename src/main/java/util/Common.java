c7d5595bbd191bc6a118f95b6c305a56ef9a2c8f
phucnn <phucnn@vng.com.vn>
2021-07-16 10:43:08 +0700

    update log4j2.xml

--- src/main/java/util/Common.java
+++ src/main/java/util/Common.java
@@ -1,5 +1,16 @@
 package util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+
+import java.io.File;
+
 public class Common {
+    static {
+        LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
+        File file = new File("config/log4j2.xml");
+        context.setConfigLocation(file.toURI());
+    }
+
     public static final LoggerManager debug = new LoggerManager("Common");
 }
--- src/main/test/TestYamlReader.java
+++ src/main/test/TestYamlReader.java
@@ -1,14 +1,19 @@
 import com.google.gson.Gson;
 import config.ListSqlTableConfig;
 import config.YAMLReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.junit.Test;
-import org.yaml.snakeyaml.Yaml;
 import util.Common;
+
+import java.io.File;
 
 public class TestYamlReader {
 
+
     @Test
     public void testRead() throws Exception {
+
 
         Common.debug.info(YAMLReader.convertYamlToJson("config/task/init.yaml"));
         ListSqlTableConfig list = new ListSqlTableConfig();
