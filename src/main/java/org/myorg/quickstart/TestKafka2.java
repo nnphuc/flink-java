729641249b21d07c67d73c9164761250445cc795
phucnn <phucnn@vng.com.vn>
2021-09-04 23:25:09 +0700

    run thread - env

--- src/main/java/org/myorg/quickstart/TestKafka2.java
+++ src/main/java/org/myorg/quickstart/TestKafka2.java
@@ -5,7 +5,6 @@ import config.ListSqlTableConfig;
 import config.SqlExecuteConfig;
 import config.SqlTableConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 import org.apache.flink.util.CloseableIterator;
@@ -22,32 +21,28 @@ public class TestKafka2 {
         LoggerManager jsonReader = new LoggerManager("JSONReader");
         jsonReader.error("--- init reader");
 
-
         // set up the streaming execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        final StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
-
-
-
         ListSqlExecuteConfig listSqlExecuteConfig = new ListSqlExecuteConfig();
-        for (SqlExecuteConfig config : listSqlExecuteConfig.listSqlExecute) {
-            try {
-                Common.logger.debug("Submit SQL:", config.sql);
-                SqlExecute.submit(tEnv, config);
-            } catch (Exception e) {
-                Common.logger.error(e);
-            }
-        }
-
         ListSqlTableConfig listSqlTableConfig = new ListSqlTableConfig();
         for (SqlTableConfig config : listSqlTableConfig.listSqlTable) {
             try {
-                Common.logger.debug("Submit SQL:", config.sql);
-                SqlTable sqlTable = SqlTable.submit(tEnv, config);
-                CloseableIterator<Row> iter = sqlTable.getTable()
-                    .execute()
-                    .collect();
                 new Thread(() -> {
+                    final StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+                    for (SqlExecuteConfig config1 : listSqlExecuteConfig.listSqlExecute) {
+                        try {
+                            Common.logger.debug("Submit SQL:", config1.sql);
+                            SqlExecute.submit(tEnv, config1);
+                        } catch (Exception e) {
+                            Common.logger.error(e);
+                        }
+                    }
+                    Common.logger.debug("Submit SQL:", config.sql);
+                    SqlTable sqlTable = SqlTable.submit(tEnv, config);
+                    CloseableIterator<Row> iter = sqlTable.getTable()
+                            .execute()
+                            .collect();
+
                     while (iter.hasNext()) {
                         Row row = iter.next();
                         Printer.printAsTableauForm(sqlTable.getTable().getSchema(), row, Common.logger);
